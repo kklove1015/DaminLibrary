3695959758d74d25a2cbd3e5d7910e6e05d11ae1
kklove1015 <kklove1015@gmail.com>
2022-03-11 21:14:35 +0900

    Delete Snackbar

--- DaminLibrary/Properties/AssemblyInfo.cs
+++ DaminLibrary/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("DaminLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DaminLibrary Sample")]
+[assembly: AssemblyDescription("C# Extansion Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Damin")]
 [assembly: AssemblyProduct("DaminLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
